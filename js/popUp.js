abeb5acae4745667aee180a0b1488e1f92a688b5
anastasia_mia <nastia.mianovska333@gmail.com>
2023-05-31 17:07:41 +0300

    adding of 'showRules' button and rules to the game

--- js/popUp.js
+++ js/popUp.js
@@ -6,6 +6,14 @@ $('.gameEnd_cross').on('click', () =>{
     $('.blurBackgroundOfGameEndPopUp').css('visibility', 'hidden');
 })
 
+$('.rules_cross').on('click', () =>{
+    $('.game_rules').removeClass('visibleBlock');
+})
+
+$('.showRulesButton').on('click', () =>{
+    $('.game_rules').toggleClass('visibleBlock');
+})
+
 $('.gameEnd_restartButton').on('click', () =>{
     $('.blurBackgroundOfGameEndPopUp').css('visibility', 'hidden');
     $('.board').html('');
